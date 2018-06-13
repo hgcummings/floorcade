0ca0c3173e5233aa4d0e6d122675d9523ff75a79
hgcummings <harry@xlr3.com>
2018-06-13 18:39:54 +0100

    Proof of concept for Dancefloor output and Gamepad input

---
+++ dancefloor.js
@@ -0,0 +1,36 @@
+const { danceFloorServer } = require('./config.json');
+const request = require('request-promise-native');
+const util = require('util');
+const net = require('net');
+
+module.exports.activate = async () => {
+    console.log('Creating server...');
+    const server = net.createServer();
+    console.log('Listening...');
+    const listen = util.promisify(server.listen).bind(server);
+    await listen();
+    const {port} = server.address();
+
+    console.log('Asking dancefloor to delegate...');
+    const config = await request.post(`http://${danceFloorServer.host}:${danceFloorServer.httpPort}/api/delegate`, {
+        json:true,
+        body: {
+            host: require('os').hostname(),
+            port: port
+        }
+    });
+
+    console.log('Awaiting connections...');
+    server.on('connection', socket => {
+        console.log('Got connection. Waiting for data...');
+        socket.on('data', (data) => {    
+            const [width, height] = data;
+
+            const frameData = new Uint8Array(width * height * 3);
+            frameData.fill(Math.floor(Math.random() * 256));
+
+            console.log('Got data. Sending frame data...');
+            socket.write(frameData);
+        });
+    });
+}
