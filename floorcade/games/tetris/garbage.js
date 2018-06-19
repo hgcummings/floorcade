8baf1d6a53d29246a7bf7f9cfb862fd4a142c6ac
hgcummings <harry@xlr3.com>
2018-06-19 22:37:30 +0100

    Simplify garbage tracking

--- floorcade/games/tetris/garbage.js
+++ floorcade/games/tetris/garbage.js
@@ -1,31 +1,27 @@
 const EventEmitter = require('events');
 const most = require('most');
 
-const add2 = (a, b) => a + b;
 const addMany = function addMany() {
-    return Array.prototype.reduce.call(arguments, add2, 0);
+    return Array.prototype.reduce.call(arguments, (a, b) => a + b, 0);
 };
 
 module.exports = class Garbage {
     constructor(players) {
-        this.emitter = new EventEmitter();
-        this.emitter.setMaxListeners(players.length ** 2);
-        this.players = players;
+        this.state = players.map(player => {
+            const emitter = new EventEmitter();
+            const stream = most.fromEvent('garbage', emitter);
+            return { player, emitter, stream };
+        });
     }
 
     update(source, garbage) {
-        this.emitter.emit('garbage', { source, garbage });
+        this.state.find(s => s.player === source).emitter.emit('garbage', garbage);
     }
 
-    consume (target) {
-        return most.combineArray(
-            addMany,
-            this.players.filter(player => player !== target).map(
-                player => most.fromEvent('garbage', this.emitter)
-                    .filter(({source}) => source === player)
-                    .map(({garbage}) => garbage)
-                    .scan((prev, curr) => curr, 0)
-            )
+    consume(target) {
+        return most.combineArray(addMany,
+            this.state.filter(s => s.player !== target)
+                .map(s => s.stream.scan((_, cur) => cur, 0))
         ).map(garbage => ({garbage}));
     }
 }
