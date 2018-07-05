ba94dc520a36df93c7290381326e3fc7707331ee
hgcummings <harry@xlr3.com>
2018-07-05 10:51:25 +0100

    [Launcher] Associated player number with each gamepad

--- input.js
+++ input.js
@@ -4,7 +4,7 @@ const HID = require('node-hid');
 const axes = [['DL','DR'], ['DU','DD']];
 const btns = ['FR','FB','FL','FT','LS','RS','SL','SR'];
 
-const activeDevices = [];
+const activePlayers = [];
 const gamepads = HID.devices().filter(device => 
     device.vendorId === 1411 && device.productId === 8288);
 
@@ -14,48 +14,50 @@ if (!gamepads.length) {
 }
 
 module.exports = (playerCount, writeStream) => {
-    while (activeDevices.length) {
-        activeDevices.pop().removeAllListeners('data');
+    while (activePlayers.length) {
+        activePlayers.pop().device.removeAllListeners('data');
     }
     
     const on = (player, button) => writeStream.write(`P${player}${button}1\n`);
     const off = (player, button) => writeStream.write(`P${player}${button}0\n`);
     
     gamepads.forEach((gamepad, index) => {
-        let player = (index % playerCount) + 1;
-        const device = new HID.HID(gamepad.path);
-        activeDevices.push(device);
+        let player = {
+            number: (index % playerCount) + 1,
+            device: new HID.HID(gamepad.path)
+        }
+        activePlayers.push(player);
     
         let prevAxes = [128, 128];
         let prevBtns = 0;
         
-        device.on('data', data => {
+        player.device.on('data', data => {
             let currAxes = [data[0], data[1]];
             for (let i = 0; i < axes.length; ++i) {
                 if (currAxes[i] !== prevAxes[i]) {
                     if (currAxes[i] < 64) {
-                        on(player, axes[i][0]);
+                        on(player.number, axes[i][0]);
                     } else if (currAxes[i] > 192) {
-                        on(player, axes[i][1]);
+                        on(player.number, axes[i][1]);
                     }
                     if (prevAxes[i] < 64) {
-                        off(player, axes[i][0]);
+                        off(player.number, axes[i][0]);
                     } else if (prevAxes[i] > 192) {
-                        off(player, axes[i][1]);
+                        off(player.number, axes[i][1]);
                     }
                 }
             }
             prevAxes = currAxes;
         });
         
-        device.on('data', data => {
+        player.device.on('data', data => {
             let currBtns = data[2];
             if (currBtns !== prevBtns) {
                 for (let i = 0, btn = 1; i < btns.length; btn <<= 1, ++i) {
                     if ((btn & currBtns) && !(btn & prevBtns)) {
-                        on(player, btns[i]);
+                        on(player.number, btns[i]);
                     } else if ((btn & prevBtns) && !(btn & currBtns)) {
-                        off(player, btns[i]);
+                        off(player.number, btns[i]);
                     }
                 }
             }
@@ -65,11 +67,11 @@ module.exports = (playerCount, writeStream) => {
         if (process.env['DEV']) {
             console.log('Using dev mode for gamepad input.');
             let prevShift = 0;
-            device.on('data', data => {
+            player.device.on('data', data => {
                 if (data[3] & 32 && !(prevShift & 32)) {
-                    player = (player % playerCount) + 1;
+                    player.number = (player.number % playerCount) + 1;
                 } else if (data[3] & 16 && !(prevShift & 16)) {
-                    player = ((player - 2 + playerCount) % playerCount) + 1;
+                    player.number = ((player.number - 2 + playerCount) % playerCount) + 1;
                 }
                 prevShift = data[3];
             });
