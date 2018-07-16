43954921ee3d24ec92f571566f537d3bbe1d1af8
Jamie Ellis <Jamie.Ellis@softwire.com>
2018-07-16 19:46:00 +0100

    [Game: SSSW] Simple pixel movement

--- games/sssw/controller.js
+++ games/sssw/controller.js
@@ -3,9 +3,7 @@
  * Nothing below is game-specific; all games will need to implement the functionality below.
  */
 const readline = require('readline');
-const modelFactory = require('./model');
 const viewFactory = require('./view');
-const rxjs = require('rxjs');
 const { filter, map } = require('rxjs/operators');
 const { ArgumentParser } = require('argparse');
 
@@ -16,30 +14,16 @@ const config = parser.parseArgs();
 
 const input = readline.createInterface(process.stdin);
 
-const playerEvents = rxjs.fromEvent(input, 'line')
-    .pipe(filter(event => event[0] === 'P'))
-    .pipe(map(event => ({
-            id: parseInt(event[1], 10),
-            key: event.substr(2, 2),
-            type: event[4] === '1' ? 'down' : 'up'
-        })));
-
-const model = modelFactory.init(config, playerEvents);
 const view = viewFactory.init(config);
 
 input.on('line', event => {
     if (event.trim() === 'STICK') {
-        process.stdout.write(view.render(model.state));
+        process.stdout.write(view.render());
     } else if (event.trim() === 'SKILL') {
         process.exit();
+    } else if (event === 'P2FR1') {
+        view.playerButtonPressed();        
     }
 });    
 
 process.stdout.write('READY\n');
-
-model.activity
-  .then(() => process.exit(0))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
--- games/sssw/view/index.js
+++ games/sssw/view/index.js
@@ -1,15 +1,43 @@
-const round = require('./round');
-const scores = require('./scores');
+const setColorOfPixel = (width, height, pixels, x, y, color) => {
+    if (x >= 0 && x < width && y >= 0 && y < height) {
+        for (let c = 0; c < 3; ++c) {
+            pixels[(((y * width) + x) * 3) + c] = color[c];            
+        }
+    }
+};
+
+class Ship {
+    constructor() {
+        this.posX = 35;
+        this.posY = 17;
+    }
+
+    render(width, height, pixels) {
+        setColorOfPixel(width, height, pixels, this.posX, this.posY, [255, 255, 255]);
+    }
+
+    playerButtonPressed() {
+        this.posX += 1;
+    }
+}
 
 module.exports.init = ({width, height}) => {
     const pixels = new Uint8Array(width * height * 3);
 
-    const render = state => {
-        scores.render({width, height}, state.scores, pixels);
+    const ship = new Ship();
+    
+    const render = () => {
+        pixels.fill(0);
+        ship.render(width, height, pixels);
         return pixels;
     }
 
+    const playerButtonPressed = () => {
+        ship.playerButtonPressed();
+    }
+
     return {
-        render
+        render,
+        playerButtonPressed
     }
 }
