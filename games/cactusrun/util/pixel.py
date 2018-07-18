47d642f9e256a3e5bc28c60fcb536f06a92f8e4f
eric-tong <erictong.designs@gmail.com>
2018-07-18 18:17:54 +0100

    [Cactus Run] Add colors

--- games/cactusrun/util/pixel.py
+++ games/cactusrun/util/pixel.py
@@ -11,3 +11,10 @@ class Pixel:
 
 white = Pixel(255, 255, 255)
 black = Pixel(0, 0, 0)
+
+red = Pixel(255, 0, 0)
+green = Pixel(0, 255, 0)
+blue = Pixel(0, 0, 255)
+yellow = Pixel(255, 255, 0)
+magenta = Pixel(255, 0, 255)
+cyan = Pixel(0, 255, 255)
