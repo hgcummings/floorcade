01f2b84b35f34644db4e76562e4f64141752cc88
eric-tong <erictong.designs@gmail.com>
2018-07-20 15:15:52 +0100

    [Cactus Run] Change avatar

--- games/cactusrun/units/runner.py
+++ games/cactusrun/units/runner.py
@@ -2,9 +2,30 @@ import settings
 from util import pixel
 from util.point import Point
 from units.unit import Unit
+from util.timer import Timer
+
+bare = False
+fill = pixel.magenta
+open_legs_pixel_map = [
+    [bare, fill, bare],
+    [bare, fill, fill],
+    [bare, fill, bare],
+    [fill, bare, fill],
+]
+closed_legs_pixel_map = [
+    [bare, fill, bare],
+    [bare, fill, fill],
+    [bare, fill, bare],
+    [bare, fill, bare],
+]
 
 
 class Runner(Unit):
+
+    def __init__(self, coords, size):
+        Unit.__init__(self, coords, size)
+        self.timer = Timer()
+        self.legs_are_open = True
 
     def in_default_position(self, dimensions):
         return self.coords.y >= dimensions.height / 2
@@ -13,13 +34,16 @@ class Runner(Unit):
         self.accelerate(Point(0, settings.jump_height * -1), 1)
 
     def get_pixels(self):
-        bare = False
-        fill = pixel.cyan
-        pixel_map = [
-            [bare, fill, bare],
-            [bare, fill, fill],
-            [bare, fill, bare],
-            [fill, bare, fill],
-        ]
+        self.legs_are_open = self.get_legs_status()
+        pixel_map = list(open_legs_pixel_map if self.legs_are_open else closed_legs_pixel_map)
         pixel_map.reverse()
         return pixel_map
+
+    def get_legs_status(self):
+        if self.velocity.y != 0:
+            return True
+        elif self.timer.elapsed_time() > 0.1:
+            self.timer.set_last_tick_to_current_time()
+            return not self.legs_are_open
+        else:
+            return self.legs_are_open
--- games/cactusrun/util/timer.py
+++ games/cactusrun/util/timer.py
@@ -6,7 +6,13 @@ class Timer:
     def __init__(self):
         self.last_tick = time.time()
 
-    def elapsed_time_since_last_check(self):
-        elapsed_time = time.time() - self.last_tick
+    def elapsed_time(self):
+        return time.time() - self.last_tick
+
+    def set_last_tick_to_current_time(self):
         self.last_tick = time.time()
+
+    def elapsed_time_since_last_check(self):
+        elapsed_time = self.elapsed_time()
+        self.set_last_tick_to_current_time()
         return elapsed_time
