929c60edd4e6e5ed1c05e72c81889d286617ec0e
Michael Kearns <michael.kearns@softwire.com>
2018-08-16 19:11:26 +0100

    [bullet] delete codez

--- games/bullet/model/index.js
+++ games/bullet/model/index.js
@@ -1,21 +1,11 @@
 const scanInput = require('./input');
 const levelFactory = require('./level');
-const scoresFactory = require('./scores');
 
 async function runGame({width, height}, state, input) {
-    let playerTotals = [0,0,0,0];
-    let topScore = 0;
+    let playerTotals = [0];
 
-    while (topScore < 25 || playerTotals.filter(total => total === topScore).length > 1) {
-        state.level = levelFactory.init({width, height}, input);
-        await state.level.activity;
-        const players = state.level.players;
-        delete state.level;
-        state.scores = scoresFactory.init(players, playerTotals);
-        playerTotals = await state.scores.activity;
-        delete state.scores;
-        topScore = Math.max(...playerTotals);
-    }
+    state.level = levelFactory.init({width, height}, input);
+    await state.level.activity;
 } 
 
 module.exports.init = ({width, height}, inputEvents) => {
